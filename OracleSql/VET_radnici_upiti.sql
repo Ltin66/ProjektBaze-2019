490da64f0e9a6475a6fa3ad958f1ed0cfcccf0ed
Tin <lord66tin66@gmail.com>
2019-11-25 14:54:25 +0100

    Update VET_radnici_upiti.sql

--- OracleSql/VET_radnici_upiti.sql
+++ OracleSql/VET_radnici_upiti.sql
@@ -16,21 +16,19 @@ SELECT ZAP.ZAPOSLENIK_ID, ZAP.IME,ZAP.PREZIME, RST.NAZIV
 
 --Upit 1 - upit koji prima id zaposlenika, mjesec i godinu te izračuna njegovu plaću za taj mjesec u toj godini
 
+--Upit 2 - vraca skoro sve podatke o zaposleniku sa ID
 
 
---Upit 2 - 
 
-
---Upit 3 - 
-
-
---View 1 - view koji prikazuje zakazane dolaske u sljedecih 7 dana "raspored"
+--View 1 - view koji prikazuje zakazane dolaske u sljedecih 7 dana "raspored",  moraju se osvježit nakon svake izmjene
 
 --View 2 - radnici, radno mjesto, i dali su na nekom odmoru u obliku datuma do kad su
+
 
 --Insert 1 - unesi novog radnika (preko funkcije), automatski inkrementira id, stavlja datum itd.. sa transakcijom
 
 --insert 2 - unesi novi termin za raspored (funkcija)
+
 
 --Update 1 - ako je dosao na posao pa updejtaj vrijeme dolaska
 
