eb06b0dc8ed60c314e6348aa284bb3266a90a77d
Tin <lord66tin66@gmail.com>
2019-11-25 14:48:00 +0100

    Update VET_radnici_upiti.sql

--- OracleSql/VET_radnici_upiti.sql
+++ OracleSql/VET_radnici_upiti.sql
@@ -3,9 +3,10 @@ select * from ZAPOSLENIK;
 SELECT * from ZAPOSLENIK join RADNI_STATUS RS on ZAPOSLENIK.ZAPOSLENIK_ID = RS.ZAPOSLENIK_ID;
 
 
+Select, insert, update i delete za radnike
 
 
---Upit 1 , vraća imena radnika i njihovo radno mjesto, ako
+--Upit vraća imena radnika i njihovo radno mjesto
 
 SELECT ZAP.ZAPOSLENIK_ID, ZAP.IME,ZAP.PREZIME, RST.NAZIV
     from ZAPOSLENIK ZAP join RADNI_STATUS RS on ZAP.ZAPOSLENIK_ID = RS.ZAPOSLENIK_ID
@@ -13,13 +14,24 @@ SELECT ZAP.ZAPOSLENIK_ID, ZAP.IME,ZAP.PREZIME, RST.NAZIV
     ORDER BY ZAP.ZAPOSLENIK_ID;
 
 
---Upit 2 - upit koji prima id zaposlenika, mjesec i godinu te izračuna njegovu plaću za taj mjesec u toj godini
+--Upit 1 - upit koji prima id zaposlenika, mjesec i godinu te izračuna njegovu plaću za taj mjesec u toj godini
 
 
 
---Upit 3 - upit koji vraca radnike (id i ime) koji su odsutni , where datum between  "01.02.2003" npr.
+--Upit 2 - 
 
 
---Upit 4 -
+--Upit 3 - 
 
---View 1 - view koji prikazuje
+
+--View 1 - view koji prikazuje zakazane dolaske u sljedecih 7 dana "raspored"
+
+--View 2 - radnici, radno mjesto, i dali su na nekom odmoru u obliku datuma do kad su
+
+--Insert 1 - unesi novog radnika (preko funkcije), automatski inkrementira id, stavlja datum itd.. sa transakcijom
+
+--insert 2 - unesi novi termin za raspored (funkcija)
+
+--Update 1 - ako je dosao na posao pa updejtaj vrijeme dolaska
+
+
