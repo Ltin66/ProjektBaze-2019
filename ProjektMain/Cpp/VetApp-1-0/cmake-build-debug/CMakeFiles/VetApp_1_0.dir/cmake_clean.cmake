9c2b3d20f2fe00277b696040b5be88e5cb27d924
Tin <lord66tin66@gmail.com>
2019-11-27 17:26:40 +0100

    cpp init

---
+++ ProjektMain/Cpp/VetApp-1-0/cmake-build-debug/CMakeFiles/VetApp_1_0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VetApp_1_0.dir/main.cpp.o"
+  "VetApp_1_0"
+  "VetApp_1_0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VetApp_1_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
