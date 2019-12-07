a1455d1ef13c3c15f8669596eea259e95adeb0df
Tin <lord66tin66@gmail.com>
2019-12-07 18:35:14 +0100

    Inspekcije .sql done

--- ProjektMain/Sql/Tablice/Veterinar_Table_Inspekcije.sql
+++ ProjektMain/Sql/Tablice/Veterinar_Table_Inspekcije.sql
@@ -1,34 +1,43 @@
 
-create table inspekcije_tip(
-  tipID INTEGER PRIMARY KEY,
-  naziv VARCHAR(20) NOT NULL,
-  cijena NUMERIC(15,2) NOT NULL
+
+-- TODO : check da je cijena > 0
+create table inspekcija_tip(
+    inspekcija_tip_id             INTEGER         NOT NULL , --auto incr
+    naziv                         VARCHAR(20)     NOT NULL ,
+    cijena                        NUMERIC(16,2)   NOT NULL ,
+    CONSTRAINT INS_PK PRIMARY KEY (inspekcija_tip_id)
 )
 /
 
-create table inspekcije(
-  inspekcijaID INTEGER PRIMARY KEY,
-  korisnikID INTEGER NOT NULL,
-  cijena_total NUMERIC(15,2),
-  FOREIGN KEY(korisnikID) REFERENCES korisnik(korisnikID)
+create table inspekcija_korisnik(
+    inspekcija_korisnik_id          INTEGER         NOT NULL , --auto incr
+    korisnik_id                     INTEGER         NOT NULL ,
+    cijena_total                    NUMERIC(15,2)   NOT NULL ,
+    CONSTRAINT INS_KOR_PK PRIMARY KEY (inspekcija_korisnik_id),
+    CONSTRAINT INS_KOR_FK FOREIGN KEY (korisnik_id) REFERENCES korisnik(korisnik_id)
 )
 /
 
-create table inspekcije_log(
-  inspekcijaID INTEGER NOT NULL,
-  tipID INTEGER NOT NULL,
-  datum DATE,
-  izvrseno BOOL,
-  opis VARCHAR(200),
-  FOREIGN KEY(inspekcijaID) REFERENCES inspekcije(inspekcijaID),
-  FOREIGN KEY(tipID) REFERENCES inspekcije_tip(tipID)
+
+-- Oracle nema bool pa se radi sa NUMERIC(1) - u koji spremamo 0 ili 1, -1 za unknown gdje može
+-- TODO check da je vrijednost izvresno 1 ili 0
+
+create table inspekcija(
+    inspekcija_id                   INTEGER         NOT NULL, --auto incr
+    inspekcija_tip_id               INTEGER         NOT NULL,
+    datum                           DATE DEFAULT TO_DATE('01.01.0001','DD.MM.YYYY') NOT NULL ,
+    opis                            CLOB DEFAULT '@' NOT NULL ,
+    CONSTRAINT INS_PK PRIMARY KEY (inspekcija_id),
+    CONSTRAINT INS_FK FOREIGN KEY (inspekcija_tip_id) REFERENCES inspekcija_tip(inspekcija_tip_id)
 )
 /
 
-create table inspekcije_radnici(
-  inspekcijaID INTEGER NOT NULL,
-  zaposlenikID INTEGER NOT NULL,
-  FOREIGN KEY(inspekcijaID) references inspekcije(inspekcijaID),
-  FOREIGN KEY(zaposlenikID) references zaposlenici(zaposlenikID)
+create table inspekcija_zaposlenik(
+    inspekcija_zaposlenik_id        INTEGER         NOT NULL , --auto incr
+    inspekcija_id                   INTEGER         NOT NULL,
+    zaposlenik_id                   INTEGER         NOT NULL,
+    CONSTRAINT INS_ZAP_PK     PRIMARY KEY (inspekcija_zaposlenik_id),
+    CONSTRAINT INS_ZAP_FK_INS FOREIGN KEY(inspekcija_id) references inspekcija(inspekcija_id),
+    CONSTRAINT INS_ZAP_FK_ZAP FOREIGN KEY(zaposlenik_id) references zaposlenik(zaposlenik_id)
 )
 /
