9ca12612b51fa1ac65ef0b1177fa5aede1e805e1
Tin <lord66tin66@gmail.com>
2019-12-08 13:12:48 +0100

    Funkcije za auto dodavanje usera i SYNONYMA

---
+++ ProjektMain/Sql/User/Veterinar_START_Users.sql
@@ -0,0 +1,19 @@
+----------------------------------------------------------------------
+--
+--                  DODAVANJE KORISNIKA BAZE
+--                      KAO VETERINAR_SYS
+--
+----------------------------------------------------------------------
+
+--CREATE USER mirkomirkec3 identified by 1234;
+--DROP USER mirkomirkec3 CASCADE /
+
+
+begin
+
+    create_user_racunovoda('mirkomirkec3','1234');
+
+    create_user_racunovoda('mirkomirkec9','1234');
+
+
+end;
