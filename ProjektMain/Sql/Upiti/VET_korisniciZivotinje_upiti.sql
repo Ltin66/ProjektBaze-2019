6894fd217de94693b74affe23f410678fe4e2cc7
Tin <lord66tin66@gmail.com>
2019-12-09 16:50:03 +0100

    Update VET_korisniciZivotinje_upiti.sql

--- ProjektMain/Sql/Upiti/VET_korisniciZivotinje_upiti.sql
+++ ProjektMain/Sql/Upiti/VET_korisniciZivotinje_upiti.sql
@@ -4,4 +4,12 @@
 
 -- Insert 3 - spoji zivotinju i korisnika
 
+-- View 1 - svi pregledi za korisnika i zivotinju, ako se umjesto zivotinje stavi "All" onda vraca sve preglede
 
+-- View 2 - sve inspekcije za korisnika
+
+-- Materializiran pogled - svi aktivni korisnici
+
+-- delete 1 - izbrisi korisnika
+
+-- delete 2 - izbrisi zivotinju
