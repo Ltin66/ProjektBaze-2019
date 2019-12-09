1b81b1382da45e6b134636ace46c9ff53914bf03
Tin <lord66tin66@gmail.com>
2019-12-09 16:39:23 +0100

    Update VET_inspekcije_upiti.sql

--- ProjektMain/Sql/Upiti/VET_inspekcije_upiti.sql
+++ ProjektMain/Sql/Upiti/VET_inspekcije_upiti.sql
@@ -1,3 +1,11 @@
 -- Select 1 - prikaži inspekcije za korisnika sa ID
 
--- Insert 1 
+-- Insert 1 - dodaj novu inspekciju, funkcija 
+
+-- Insert 2 - dodaj doktore za inspekciju
+
+-- View 1 - prikazi zakazane inspekcije
+
+-- View2 - prikazi inspekcije od datuma n do dtauma m
+
+-- Update 1 - izmjeni opis za inspekciju sa ID
