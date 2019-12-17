3069a95fa3b3ccd91d83c213b986e56f28c1d3f1
Tin <lord66tin66@gmail.com>
2019-12-17 17:34:23 +0100

    Update Racunovoda.sql

--- ProjektMain/Sql/Upiti/Racunovoda.sql
+++ ProjektMain/Sql/Upiti/Racunovoda.sql
@@ -1,28 +1,27 @@
-1. placa zaposlenika za n-ti mjesec u m-toj godini 
+1. placa zaposlenika ID za n-ti mjesec u m-toj godini procedura
 (TODO )
 
-2. unos novog zaposlenika 
+2. unos novog zaposlenika procedura
 (inser into zaposlenik (<Uneseno ime> <-||- Srednje Ime> <-||- prezime > ..... ))
 
-3. unos rasporeda
+3. unos rasporeda procedura
 (za zaposlenika ID unesi u raspred novi termin koje je : 
 <UnsesnoVijelem dolaska> VrijemeOdlaska = <Uneseno Vrijeme Dolaska > + <Unsesni sati koje treba odraditi >)
 
 
-4. pregled rasporeda za sljedecih 7 dana (View)
+4. pregled rasporeda za sljedecih 7 dana (View
 (create view as dkkdk select * from raspored where datum >= CurrentDate - 7 dana)
 
-5. update vrijeme dolaska
+5. update vrijeme dolaska procedura
 update viejme dolaska za dolazak ID
 
-6. izracunaj cijenu za pregled ID
+6. izracunaj cijenu za pregled ID procedura
 za pregled ID vrati mi njegovu cijenu
 
-7. izracunaj cijenu za inspekciju ID
+7. izracunaj cijenu za inspekciju ID procedura
 za inspekcijju ID vrati mi njegovu Cijenu
 
 
-mOždA kAsnIjE
 
-8. za svakog korisnika vrati njegovu kupnu cijne+u svih pregleda u ambulanti
-9. kao 8. samo za insekcije
+8. za svakog korisnika vrati njegovu kupnu cijne+u svih pregleda u ambulanti od jedong do drugog datuma procedura
+9. kao 8. samo za insekcije procedura
