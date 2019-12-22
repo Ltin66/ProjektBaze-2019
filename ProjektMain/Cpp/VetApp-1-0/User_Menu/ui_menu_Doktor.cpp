d9a2a923bcc3128f5d2e699643a7530598c05beb
Tin <lord66tin66@gmail.com>
2019-12-22 21:23:16 +0100

    doktor_meni skoro gotov

--- ProjektMain/Cpp/VetApp-1-0/User_Menu/ui_menu_Doktor.cpp
+++ ProjektMain/Cpp/VetApp-1-0/User_Menu/ui_menu_Doktor.cpp
@@ -26,25 +26,33 @@ int uiUserDoktorMainMenu(SAConnection &con,korisnik &kor){
     while(true) {
         int odabir = 0;
 
+        //OK znaci da je napravljen, neznaci da radi kako treba
+
         ui_print("Izbornik");
 
         ui_print("1 - Informacije o Ambulanti");
 
-        ui_print("2 - Zakazani pregledi");
+        ui_print("2 - Zakazani pregledi"); //fali procedura
         ui_print("3 - Dodavanje pregleda"); //OK
         ui_print("4 - Izmjena opisa pregleda"); //OK
 
-        ui_print("5 -  Dodavanje korisnika");
-        ui_print("6 -  Brisanje korisnika");
+        ui_print("5 -  Dodavanje korisnika"); //OK
+        ui_print("6 -  Brisanje korisnika"); //OK
 
-        ui_print("7 -  Dodavanje zivotinje");
-        ui_print("8 -  Brisanje zivotinje");
+        ui_print("7 -  Dodavanje zivotinje"); //OK
+        ui_print("8 -  Brisanje zivotinje");  //fali procedura
 
         ui_print("9 -  Spajanje Zivotinje i korisnika"); //OK
 
-        ui_print("11 - Ispis pregleda za pojedinog Korinsika");
+        ui_print("11 - Ispis pregleda za pojedinog Korinsika"); //fali procedura
         ui_print("12 - Prikaz zaposlenika"); //onaj mat_view gjde se manje vidi
 
+        ui_print("13 -  Prikaz Tipova Zivotinja"); //select
+        ui_print("14 -  Prikaz Usluga"); //select
+
+        ui_print("15 -  Dodavanje Doktora na Pregled"); //OK
+
+        ui_print("16 -  Prikaz svih ID-a Korisnika i Zivotinja za  Korisnik ID"); //MAT view
 
         ui_print("0 -  Izlaz iz Programa");
         ui_print("10 - Izlaz u Glavni Izbornik");
@@ -55,35 +63,10 @@ int uiUserDoktorMainMenu(SAConnection &con,korisnik &kor){
         cin>>odabir;
 
         if(odabir == 1){
-            SACommand cmd(&con);
-            cmd.setCommandText("selectAmbulanta");
-            //SAParam a1 = 0.0
-
-            int tmp;
-
-            ui_print("Unesite ID Ambulante za Informacije ");
-            ui_input();
-            cin>>tmp;
-
-            ui_separator();
-
-            cmd.Param("p_id").setAsNumeric() = tmp + 0.0;
-
-
-            try {
-                cmd.Execute();
-                cout<<cmd.Param("o_usluga").asLong();
-
-
-            }
-            catch(SAException & x) {printf("%s\n", (const char*)x.ErrText()); }
-
-
 
         }
         else if(odabir == 2){
-            //unesite ID zaposlenika ->
-            //pozpvi funkicju i ispisi
+
         }
         else if(odabir == 3){
             ui_print("Unesite ID usluge ");
@@ -129,6 +112,9 @@ int uiUserDoktorMainMenu(SAConnection &con,korisnik &kor){
             ui_separator();
             ui_print("OK");
 
+                try {  cmd.Execute(); }
+                catch(SAException & x) {printf("%s\n", (const char*)x.ErrText()); }
+
             }
         }
         else if(odabir == 4){
@@ -154,6 +140,9 @@ int uiUserDoktorMainMenu(SAConnection &con,korisnik &kor){
             ui_clear();
             ui_separator();
             ui_print("OK");
+
+                try {  cmd.Execute(); }
+                catch(SAException & x) {printf("%s\n", (const char*)x.ErrText()); }
 
             }
 
@@ -212,25 +201,119 @@ int uiUserDoktorMainMenu(SAConnection &con,korisnik &kor){
 
                 cmd.setCommandText("insertKORISNIK");
 
-                //cmd.Param("p_id").setAsNumeric() = ID + 0.0;
-                //cmd.Param("p_opis").setAsString() = opis.c_str();
+                cmd.Param("p_korisnik_id").setAsNumeric() =  0.0;
+                cmd.Param("p_ime").setAsString() = ime.c_str();
+                cmd.Param("p_srednje_ime").setAsString() = sred_ime.c_str();
+                cmd.Param("p_prezime").setAsString() = prezime.c_str();
+                cmd.Param("p_jmbg").setAsString() = jmbg.c_str();
+
+                cmd.Param("p_sifra").setAsNumeric() =  sifra + 0.0;
+                cmd.Param("p_datum_poc").setAsDateTime() =  datum;
+
+                cmd.Param("p_opis").setAsString() = opis.c_str();
+                cmd.Param("p_aktivan").setAsNumeric() =  1.0;
 
                 ui_clear();
                 ui_separator();
                 ui_print("OK");
+
+                try {  cmd.Execute(); }
+                catch(SAException & x) {printf("%s\n", (const char*)x.ErrText()); }
 
             }
 
 
         }
         else if(odabir == 6){
+            ui_print("Unesite ID Korisnika");
+            ui_input();
+            int id;
+            cin>>id;
 
+            SACommand cmd(&con);
+
+            cout<<"Jeste lis zadovoljni sa unosom ?";
+            if(ui_confirm()){
+
+                cmd.setCommandText("deleteKORISNIK");
+
+                cmd.Param("p_korisnik_id").setAsNumeric() = id + 0.0;
+
+                ui_clear();
+                ui_separator();
+                ui_print("OK");
+
+                try {  cmd.Execute(); }
+                catch(SAException & x) {printf("%s\n", (const char*)x.ErrText()); }
+
+            }
         }
         else if(odabir == 7){
+            ui_print("Unesite Tip Zivotinje");
+            ui_input();
+            int tip;
+            cin>>tip;
+
+            ui_print("Unesite Ime Zivotinje");
+            ui_input();
+            string ime;
+
+
+            ui_print("Zelite li opis ");
+            string opis = "";
+            if(ui_confirm()){
+                ui_input();
+                cin>>opis;
+            }
+
+            SACommand cmd(&con);
+
+            cout<<"Jeste lis zadovoljni sa unosom ?";
+            if(ui_confirm()){
+
+                cmd.setCommandText("INSERTZIVOTINJA");
+
+                cmd.Param("p_ziv_id").setAsNumeric() =  0.0;
+                //cmd.Param("p_opis").setAsString() = opis.c_str();
+                cmd.Param("p_ziv_tip_id").setAsNumeric() = tip + 0.0;
+                cmd.Param("p_ime").setAsString() = ime.c_str();
+                cmd.Param("p_opis").setAsString() = opis.c_str();
+
+
+                ui_clear();
+                ui_separator();
+                ui_print("OK");
+
+                try {  cmd.Execute(); }
+                catch(SAException & x) {printf("%s\n", (const char*)x.ErrText()); }
+
+            }
 
         }
         else if(odabir == 8){
 
+            ui_print("Unesite ID Zivotinje");
+            ui_input();
+            int id;
+            cin>>id;
+
+            SACommand cmd(&con);
+
+            cout<<"Jeste lis zadovoljni sa unosom ?";
+            if(ui_confirm()){
+
+                cmd.setCommandText("deleteZivotinja");
+
+                cmd.Param("p_korisnik_id").setAsNumeric() = id + 0.0;
+
+                ui_clear();
+                ui_separator();
+                ui_print("OK");
+
+                try {  cmd.Execute(); }
+                catch(SAException & x) {printf("%s\n", (const char*)x.ErrText()); }
+
+            }
         }
         else if(odabir == 9){
             ui_print("Unesite ID korisnika : ");
@@ -258,8 +341,56 @@ int uiUserDoktorMainMenu(SAConnection &con,korisnik &kor){
                 ui_separator();
                 ui_print("OK");
 
+                try {  cmd.Execute(); }
+                catch(SAException & x) {printf("%s\n", (const char*)x.ErrText()); }
+
             }
 
+        }
+        else if(odabir == 11){
+
+        }
+        else if(odabir == 12){
+
+        }
+        else if(odabir == 13){
+
+        }
+        else if(odabir == 14){
+
+        }
+        else if(odabir == 15){
+
+            ui_print("Unesite ID doktora");
+            ui_input();
+            int zapId;
+            cin>>zapId;
+
+            ui_print("Unesite ID Ambulante");
+            ui_input();
+            int ambId;
+            cin>>ambId;
+
+
+            SACommand cmd(&con);
+
+            cout<<"Jeste lis zadovoljni sa unosom ?";
+            if(ui_confirm()){
+
+                cmd.setCommandText("insertDOKTOR_AMBULANTA");
+
+                cmd.Param("p_amb_dok_id").setAsNumeric() =  0.0;
+                cmd.Param("p_amb_id").setAsNumeric() =  ambId + 0.0;
+                cmd.Param("p_zap_id").setAsNumeric() =  zapId +0.0;
+
+                ui_clear();
+                ui_separator();
+                ui_print("OK");
+
+                try {  cmd.Execute(); }
+                catch(SAException & x) {printf("%s\n", (const char*)x.ErrText()); }
+
+            }
         }
 
         if(odabir == 0) return 0;
