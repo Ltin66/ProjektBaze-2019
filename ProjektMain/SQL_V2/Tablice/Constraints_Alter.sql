6876f9260ffd3aff1c24c2c7caa909f263244962
Tin <lord66tin66@gmail.com>
2019-12-24 12:02:08 +0100

    alter constr

--- ProjektMain/SQL_V2/Tablice/Constraints_Alter.sql
+++ ProjektMain/SQL_V2/Tablice/Constraints_Alter.sql
@@ -1,5 +1,20 @@
+ 
 
--- Dodao 'ON DELETE CASCADE'
+--1. Brisanjem (korisnik.korisnik_id PK) briše se automatski i (korisnik_zivotinja.korisnik_id FK)
+
+ALTER TABLE KORISNIK_ZIVOTINJA
+DROP CONSTRAINT KOR_ZIV_FK_KOR;
+
+
+ALTER TABLE KORISNIK_ZIVOTINJA
+ADD CONSTRAINT KOR_ZIV_FK_KOR
+FOREIGN KEY (KORISNIK_ID)
+REFERENCES KORISNIK (KORISNIK_ID)
+ON DELETE CASCADE;
+
+------------------------------------------------------------------------------------------------------------------------
+--2.Brisanjem (zivotinja.zivotinja_id PK) briše se automatski i (korisnik_zivotinja.zivotinja_id FK)
+
 ALTER TABLE KORISNIK_ZIVOTINJA
 DROP CONSTRAINT KOR_ZIV_FK_ZIV;
 
@@ -9,4 +24,4 @@ ADD CONSTRAINT KOR_ZIV_FK_ZIV
 FOREIGN KEY (ZIVOTINJA_ID)
 REFERENCES ZIVOTINJA (ZIVOTINJA_ID)
 ON DELETE CASCADE;
-
+------------------------------------------------------------------------------------------------------------------------
