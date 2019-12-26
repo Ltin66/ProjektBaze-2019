b49dac9dba77b4b51fd4611d6b206bad669518c3
Tin <lord66tin66@gmail.com>
2019-12-26 18:18:10 +0100

    alter cascader za amb

--- ProjektMain/SQL_V2/Tablice/Constraints_Alter.sql
+++ ProjektMain/SQL_V2/Tablice/Constraints_Alter.sql
@@ -25,3 +25,35 @@ FOREIGN KEY (ZIVOTINJA_ID)
 REFERENCES ZIVOTINJA (ZIVOTINJA_ID)
 ON DELETE CASCADE;
 ------------------------------------------------------------------------------------------------------------------------
+
+
+-- komentar
+
+
+ALTER TABLE ambulanta_korisnik_zivotinja
+DROP CONSTRAINT AMB_KOR_FK_AMB;
+
+
+ALTER TABLE ambulanta_korisnik_zivotinja
+ ADD   CONSTRAINT AMB_KOR_FK_AMB FOREIGN KEY(ambulanta_id) REFERENCES ambulanta(ambulanta_id)
+ON DELETE CASCADE;
+
+--OBA SLUŽE DA BI SE MOGLA BRISATI AMBULANTA  ili?
+
+ALTER TABLE ambulanta_zaposlenik
+DROP CONSTRAINT AMB_ZAP_FK_AMB;
+
+
+ALTER TABLE ambulanta_zaposlenik
+ADD             CONSTRAINT AMB_ZAP_FK_AMB FOREIGN KEY(ambulanta_id) REFERENCES ambulanta(ambulanta_id)
+ON DELETE CASCADE;
+
+
+
+
+
+--TEST-- PROBAO radi sve ok
+select  * FROM AMBULANTA;
+delete from AMBULANTA where AMBULANTA_ID = 1;
+
+SELECT * FROM AMBULANTA_ZAPOSLENIK;
